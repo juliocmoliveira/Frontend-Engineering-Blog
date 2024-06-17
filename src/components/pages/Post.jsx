f5928c214c6cf12e3ff0ddb76498755defbcad5e
juliocmoliveira <juliocesarele@gmail.com>
2024-06-17 11:18:56 -0300

    feat: Finalize component Post

--- src/components/pages/Post.jsx
+++ src/components/pages/Post.jsx
@@ -1,21 +1,53 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
 
 import Layout from "../Layout";
+import { contentfulClient } from "../../utils/createContentfulClient";
 
 function Post() {
+
+    const { slug } = useParams();
+    const [ post, setPost ] = useState(null);
+
+    const getPost = async () => {
+        try {
+            const response = await contentfulClient.getEntries({
+                content_type: 'blogPost',
+                'fields.blogPostSlug': slug,
+            });
+
+            console.log(response);
+            setPost(response.items[0] || null);
+        } catch (error) {
+            console.log('Erro ao carregar itens do blogPost', error)
+            setPost(null);
+        }
+    }
+
+    useEffect( () => {
+        getPost();
+    }, [] );
+
     return (
         <Layout>
             <main className="container my-4">
                 <div className="row">
                     <div className="col">
-                        <h1>Título do Post</h1>
-                        <div>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel necessitatibus dolore facilis id molestiae optio aut in, cupiditate reprehenderit nihil ipsum quidem inventore nam commodi totam obcaecati incidunt, error blanditiis.
-                        </div>
+                        { !post && <div> Carregando... </div> }
 
-                        <Link to="/" className="btn btn-primary mt-4">
-                            Voltar para a home
-                        </Link >
+                        {post && (
+                            <>
+                                <h1> { post.fields.blogPostTitle } </h1>
+                                    <div 
+                                        dangerouslySetInnerHTML={{ __html: documentToHtmlString( post.fields.blogPostContent )}}
+                                    />
+                                    
+                                    <Link to="/" className="btn btn-primary mt-4">
+                                        Voltar para a home
+                                    </Link >
+                            </>
+                        )}
                     </div>
                 </div>
             </main>
